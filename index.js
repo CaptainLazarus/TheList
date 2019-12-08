d684ca3aa7a4f095c9fce4425b91cf9c24169514
CaptainLazarus <adityagudimetla@gmail.com>
2019-12-08 06:30:52 +0530

    Loads data. Add add button

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const electron = require('electron');
 const {app, BrowserWindow} = require('electron');
+const fs = require('fs');
+
+var list;
 
 function createWindow () {
     let win = new BrowserWindow({
@@ -14,3 +17,24 @@ function createWindow () {
 }
 
 app.on('ready' , createWindow);
+
+function createList(x){
+    for (var i in x){
+        var ele = document.createElement("p");
+        var t = document.createTextNode(x[i]);
+        ele.appendChild(t);
+        var div = document.getElementById("mainBody");
+        div.appendChild(ele);
+    }
+}
+
+function getDatabase(id) {
+    fs.readFile(__dirname + '/data.json' , function getData(err , data) {
+        if(err){
+            throw err;
+        } 
+        var x = JSON.parse(data);
+        x = x[id];
+        createList(x);
+    });
+}
