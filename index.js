6b92f7b0e1334c2fee9df52a00997747e803ea0d
CaptainLazarus <adityagudimetla@gmail.com>
2019-12-09 16:42:22 +0530

    Add button working

--- index.js
+++ index.js
@@ -4,27 +4,49 @@ const fs = require('fs');
 
 var list;
 
-function createWindow () {
-    let win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-
-    win.loadFile("index.html")
+function addWindow(){
+    location.assign("add.html");
 }
 
-app.on('ready' , createWindow);
+function addToList() {
+    var x , type;
+    var radioGroup = document.getElementsByName("category");
+    var text = document.getElementById("tf").value;
+    // alert(text);
+    var i;
+    for (i=0 ; i < radioGroup.length ; i++){
+        if(radioGroup[i].checked){
+            type = radioGroup[i].value;
+            // alert(type);
+        }
+    }
 
-function createList(x){
-    for (var i in x){
-        var ele = document.createElement("p");
-        var t = document.createTextNode(x[i]);
-        ele.appendChild(t);
+    fs.readFile(__dirname + '/data.json' , function getData(err , data) {
+        if(err){
+            throw err;
+        } 
+        x = JSON.parse(data);
+        // alert(JSON.stringify(x));
+        x[type].push(text);
+        // alert(JSON.stringify(x));
+        fs.writeFileSync(__dirname + '/data.json' ,  JSON.stringify(x));
+    });
+}
+
+function createList(list){
+    
+    var body = document.getElementById("mainBody");
+    var item=0;
+    while (body.childElementCount){
+        body.removeChild(body.childNodes[item]);    
+    }
+
+    for (item in list){
+        var para = document.createElement("p");
+        var text = document.createTextNode(list[item]);
+        para.appendChild(text);
         var div = document.getElementById("mainBody");
-        div.appendChild(ele);
+        div.appendChild(para);
     }
 }
 
@@ -38,3 +60,18 @@ function getDatabase(id) {
         createList(x);
     });
 }
+
+function createWindow () {
+    let win = new BrowserWindow({
+        width: 1200,
+        height: 1200,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile("index.html");
+}
+
+app.on('ready' , createWindow);
